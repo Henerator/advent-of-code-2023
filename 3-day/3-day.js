fef0b5a794f15c8df28009067cf131485093f7a8
roman_makarov <whiteblacks@yandex.ru>
2023-12-04 19:48:56 +0400

    3-day part 2

--- 3-day/3-day.js
+++ 3-day/3-day.js
@@ -49,4 +49,43 @@ function getLinesSum(lines) {
   return lines.reduce((sum, _line, index) => sum + getLineSum(lines, index), 0);
 }
 
-console.log(getLinesSum(input));
+// console.log(getLinesSum(input));
+
+// Part 2
+
+function getGearAdjacentNumbers(lines, lineIndex, gearCol) {
+  const numbers = [];
+  const rows = lines.length;
+  const gearLeft = gearCol - 1;
+  const gearRight = gearCol + 1;
+
+  for (let row = lineIndex - 1; row <= lineIndex + 1; row++) {
+    if (row < 0 || row >= rows) continue;
+
+    const line = lines[row];
+    [...line.matchAll(/\d+/g)].forEach((match) => {
+      const value = match[0];
+      const numberLeft = match.index;
+      const numberRight = match.index + value.length - 1;
+
+      if (numberLeft <= gearRight && numberRight >= gearLeft) {
+        numbers.push(Number(value));
+      }
+    });
+  }
+
+  return numbers;
+}
+
+function getLineGearsSum(lines, lineIndex) {
+  return [...lines[lineIndex].matchAll(/\*/g)].reduce((sum, match) => {
+    const gearNumbers = getGearAdjacentNumbers(lines, lineIndex, match.index);
+    return gearNumbers.length === 2 ? sum + gearNumbers[0] * gearNumbers[1] : sum;
+  }, 0);
+}
+
+function getLinesGearsSum(lines) {
+  return lines.reduce((sum, _line, index) => sum + getLineGearsSum(lines, index), 0);
+}
+
+console.log(getLinesGearsSum(input));
