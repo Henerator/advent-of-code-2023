1266a52b5896bf9813fc355907e495156de9694e
roman_makarov <whiteblacks@yandex.ru>
2023-12-04 19:48:56 +0400

    4-day part 1

---
+++ 4-day/4-day.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+const testInput = [
+  'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53',
+  'Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19',
+  'Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1',
+  'Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83',
+  'Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36',
+  'Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11',
+];
+
+const input = fs.readFileSync('./4-day/input.txt', 'utf-8').split(/\r?\n/).filter(Boolean);
+
+// Part 1
+
+function numbersLineToArr(numbersLine) {
+  return [...numbersLine.match(/\d+/g)];
+}
+
+function parseLine(line) {
+  const [winLine, yourLine] = line.split(':')[1].split('|');
+  return {
+    winNumbersSet: new Set(numbersLineToArr(winLine)),
+    numbers: numbersLineToArr(yourLine),
+  };
+}
+
+function getLinePoints(line) {
+  const lineData = parseLine(line);
+  const matchesCount = lineData.numbers.reduce(
+    (matches, number) => (lineData.winNumbersSet.has(number) ? matches + 1 : matches),
+    0
+  );
+  return matchesCount === 0 ? 0 : Math.pow(2, matchesCount - 1);
+}
+
+function getLinesPoints(lines) {
+  return lines.reduce((points, line) => points + getLinePoints(line), 0);
+}
+
+console.log(getLinesPoints(input));
